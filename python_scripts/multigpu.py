da0c6dc0160044793d1809adc976dbce7055b35f
hugorodet-poly <hugo.rodet@polymtl.ca>
2023-11-20 17:36:44 -0500

    changed backbone

--- python_scripts/multigpu.py
+++ python_scripts/multigpu.py
@@ -11,16 +11,28 @@ import torchvision.transforms as T
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(784, 256)
-        self.fc2 = nn.Linear(256, 128)
-        self.fc3 = nn.Linear(128, 10)
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
-        x = x.view(-1, 784)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
 
 # Create an instance of the model
 model = nn.DataParallel(Net())
@@ -33,7 +45,7 @@ optimizer = optim.SGD(model.parameters(), lr=0.01)
 if torch.cuda.is_available():
     device = torch.device("cuda")
     model = model.to(device)
-    print("Training on", torch.cuda.device_count(), "GPUs")
+    print("Training on", torch.cuda.device_count(), "GPU(s)")
     for i in range(torch.cuda.device_count()):
         print('|\t', torch.cuda.get_device_name(i))
 
